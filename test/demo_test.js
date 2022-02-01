86ef447b47ee18a6e53b9b916dd089fc0d043772
abhicoder0105 <sharmaabhishek9770@gmail.com>
2022-02-02 01:31:43 +0530

    configure mocha for test

---
+++ test/demo_test.js
@@ -0,0 +1,14 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// describe a test
+
+describe('some demo test', function () {
+
+  // create a test
+  it('adds two numbers', function(){
+    assert(2+3 === 5)
+  })
+
+
+})
